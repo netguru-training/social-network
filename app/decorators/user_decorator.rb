3a618e08fac9860634a3a669b60e99cca35a4337
Paweł Łaski <plaski@raffine.eu>
2014-11-15 13:28:38 +0100

    add location decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,10 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def image
-    info['image'] || '' if info
+    info.image || '' if info
+  end
+
+  def location
+    info.location if info
   end
 end
