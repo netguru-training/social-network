1bf9fc60d1b4ac2f1142585752bcf6fb1bae611b
marta <sekulic.radosav@yahoo.com>
2014-11-15 15:37:53 +0100

    adding geocoder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def description_substring(description)
+    if description.length > 15
+    "#{description[0..15]} ..."
+    else
+    "#{description}"
+    end
+  end 
 end
