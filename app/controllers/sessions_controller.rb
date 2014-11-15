05656cf6f8c10fcd66f1be07669cbd599313940c
Paweł Łaski <plaski@raffine.eu>
2014-11-15 12:03:47 +0100

    update user, sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url
+    auth = request.env['omniauth.auth']
+    user = User.where(provider: auth.provider, uid: auth.uid.to_s).first || User.create_with_omniauth(auth)
+    user.update! info: auth.info
+    reset_session
+    session[:user_id] = user.id.to_s
+    respond_to do |format|
+      flash[:notice] = 'Signed in!'
+      format.html { redirect_to root_url }
+    end
   end
 
   def destroy
