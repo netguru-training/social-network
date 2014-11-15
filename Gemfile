9c3cd2c0e4b3c30e795836e1a73d0e307da490ac
Szymon Soppa <sz.soppa@gmail.com>
2014-11-15 13:31:40 +0100

    Changes layout of welcome page

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'sass-rails', '~> 4.0.3'
 
 gem 'bootstrap-sass'
 
+gem "font-awesome-rails"
+
 gem 'autoprefixer-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115094405) do
+ActiveRecord::Schema.define(version: 20141115111022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20141115094405) do
     t.datetime "updated_at"
   end
 
+  create_table "groups", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "groups", ["category_id"], name: "index_groups_on_category_id", using: :btree
+
   create_table "users", force: true do |t|
     t.string   "provider"
     t.string   "uid"
@@ -33,6 +43,7 @@ ActiveRecord::Schema.define(version: 20141115094405) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "info"
   end
 
 end
