944b07d5124f548d92c34a8805805441154017b7
Paweł Łaski <plaski@raffine.eu>
2014-11-16 12:18:26 +0100

    update geolocator

--- app/services/geolocator.rb
+++ app/services/geolocator.rb
@@ -1,16 +1,16 @@
 class Geolocator
-  def initialize(user, session = nil)
+  def initialize(user, session = nil, request)
     if session.try(:geolocation)
       geo = session[:geolocation]
       @latitude = geo[:lat]
       @longitude = geo[:long]
     elsif user.try(:location)
-      location = Geocoder.search(user.location).coordinates
+      location = Geocoder.search(user.location)[0].coordinates
       @latitude = location[0]
       @longitude = location[1]
     else
       ip = request.remote_ip == '127.0.0.1' ? '187.34.44.23' : request.remote_ip
-      location = Geocoder.search(ip).coordinates
+      location = Geocoder.search(ip)[0].coordinates
       @latitude = location[0]
       @longitude = location[1]
     end
