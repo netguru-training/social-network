361a8c4421a7c7b9daae6cd2ea6bb3d10307718b
Szymon Soppa <sz.soppa@gmail.com>
2014-11-16 14:01:30 +0100

    Fixes problem of unsigned user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :authenticate_user, except: [:sessions]
+  
   expose(:categories)
   expose(:category)
   expose(:events) { category.events}
