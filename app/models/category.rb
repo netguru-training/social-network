6bf6f5d5cb511acb43360b9fc1dcbd07c1025d30
zmateusz <mat309@o2.pl>
2014-11-16 10:04:43 +0100

    add category_id to Events

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   has_many :groups
+  has_many :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115144029) do
+ActiveRecord::Schema.define(version: 20141116090151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20141115144029) do
     t.datetime "photo_updated_at"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "category_id"
   end
+
+  add_index "events", ["category_id"], name: "index_events_on_category_id", using: :btree
 
   create_table "groups", force: true do |t|
     t.string   "name"
